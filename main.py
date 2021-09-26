5d2a3bc3775e7fce739c2af72e32e7e9879d2651
DyakonovDmitriy <ddyakonov82@outlook.com>
2021-09-26 17:26:28 +0300

    trying_hard

--- main.py
+++ main.py
@@ -2,3 +2,24 @@ import requests
 import json
 
 url='https://www.mail.ru'
+print('hello', 'world', 123, sep=':', end=' !!! \n')
+st='Марка авто'
+age=16
+print(type(st))
+print(type(age))
+engain_volume=1.6
+print(type(engain_volume))
+a=True
+print(type(a))
+
+a=int(input('Введите первое число: '))
+b=int(input('Введите второе число: '))
+print(a+b)
+print(a*b)
+print(a-b)
+print(a**b)
+print(a>b)
+
+'''
+comments
+'''
